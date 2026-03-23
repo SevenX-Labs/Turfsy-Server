b35d161fb355dd440c0329a7dfb0df595148499d
Sahil-Hode <sahilhode67@gmail.com>
2026-03-23 15:35:07 +0530

    add user profile module with controller, service, and DTOs

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { UserProfileModule } from './modules/user-profile/user-profile.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule],
+  imports: [PrismaModule, AuthModule, UserProfileModule],
   controllers: [AppController],
   providers: [AppService],
 })
