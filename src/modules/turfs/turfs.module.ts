ddd2aff57071e79f9f0b75c7bbb00faed5f02913
Sahil-Hode <sahilhode67@gmail.com>
2026-03-26 21:33:16 +0530

    new changes

--- src/modules/turfs/turfs.module.ts
+++ src/modules/turfs/turfs.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TurfsService } from './turfs.service';
 import { TurfsController } from './turfs.controller';
 import { PrismaModule } from '../../prisma/prisma.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule],
   controllers: [TurfsController],
   providers: [TurfsService],
 })
