0b7bc796f231ecd4b6ae44543199d8ba4b989cb4
Sahil-Hode <sahilhode67@gmail.com>
2026-04-04 23:50:43 +0530

    feat: import AuthModule into UserGamificationModule to enable authentication support

--- src/modules/user-gamification/user-gamification.module.ts
+++ src/modules/user-gamification/user-gamification.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserGamificationService } from './user-gamification.service';
 import { UserGamificationController } from './user-gamification.controller';
 import { PrismaModule } from '../../prisma/prisma.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule],
   controllers: [UserGamificationController],
   providers: [UserGamificationService],
   exports: [UserGamificationService],
