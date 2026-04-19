e176b3e0dd7dfdd72d4fc7038a6813c2131aaed2
Sahil-Hode <sahilhode67@gmail.com>
2026-04-19 10:40:05 +0530

    feat: import AuthModule into NotificationsModule to enable authentication support

--- src/common/notifications/notifications.module.ts
+++ src/common/notifications/notifications.module.ts
@@ -3,9 +3,10 @@ import { NotificationsService } from './notifications.service';
 import { PrismaModule } from '../../prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { NotificationsController } from './notifications.controller';
+import { AuthModule } from '../../modules/auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, ConfigModule],
+  imports: [PrismaModule, ConfigModule, AuthModule],
   providers: [NotificationsService],
   controllers: [NotificationsController],
   exports: [NotificationsService],
