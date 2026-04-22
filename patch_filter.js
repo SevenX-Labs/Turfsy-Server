c7bf2ff0afca2a1afd5096094fd8a0d84a207744
Sahil-Hode <sahilhode67@gmail.com>
2026-04-22 19:41:35 +0530

    chore: remove legacy patch_filter.js script

--- patch_filter.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const file = 'src/common/filters/security-exception.filter.ts';
-let code = fs.readFileSync(file, 'utf8');
-code = code.replace(
-  /const clientMessage =\s+status >= 500\s+\? GENERIC_MESSAGES\[status\] \|\| 'Something went wrong.'\s+: originalMessage;/g,
-  'const clientMessage = originalMessage;'
-);
-fs.writeFileSync(file, code);
