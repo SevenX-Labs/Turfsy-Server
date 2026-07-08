eac95f8667746f8333986ea70dc5adac5c1884ed
Sahil-Hode <sahilhode67@gmail.com>
2026-07-08 13:16:39 +0530

    feat: import AuthModule into OwnerSettlementsModule to enable authentication support

--- src/modules/owner-settlements/owner-settlements.module.ts
+++ src/modules/owner-settlements/owner-settlements.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../../prisma/prisma.module';
+import { AuthModule } from '../auth/auth.module';
 import { OwnerSettlementsController } from './owner-settlements.controller';
 import { OwnerSettlementsService } from './owner-settlements.service';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule],
   controllers: [OwnerSettlementsController],
   providers: [OwnerSettlementsService],
   exports: [OwnerSettlementsService],
