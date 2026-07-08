b41771c08e9f05b43c9732098617bd6f541e9c1d
Sahil-Hode <sahilhode67@gmail.com>
2026-07-08 21:08:05 +0530

    feat: apply SkipThrottle decorator to UserHomeController to bypass rate limiting

--- src/modules/user-home/user-home.controller.ts
+++ src/modules/user-home/user-home.controller.ts
@@ -7,6 +7,7 @@ import {
   HttpCode,
   HttpStatus,
 } from '@nestjs/common';
+import { SkipThrottle } from '@nestjs/throttler';
 import { Request } from 'express';
 import { UserHomeService } from './user-home.service';
 import { UserHomeResponseDto } from './dto/user-home-response.dto';
@@ -24,6 +25,7 @@ interface AuthenticatedRequest extends Request {
 
 @ApiTags('Users')
 @ApiBearerAuth('JWT-auth')
+@SkipThrottle()
 @Controller('api/v3/user-home')
 export class UserHomeController {
   constructor(private readonly userHomeService: UserHomeService) {}
